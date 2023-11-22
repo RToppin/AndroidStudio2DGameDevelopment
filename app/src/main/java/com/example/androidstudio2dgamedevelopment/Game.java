3334652f13dfe71dbfd0535ecb1131823cfc59cd
RToppin <ryan.toppin3408@gmail.com>
2023-11-22 15:27:48 -0500

    Player class

--- app/src/main/java/com/example/androidstudio2dgamedevelopment/Game.java
+++ app/src/main/java/com/example/androidstudio2dgamedevelopment/Game.java
@@ -3,6 +3,7 @@ package com.example.androidstudio2dgamedevelopment;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -15,8 +16,9 @@ import androidx.core.content.ContextCompat;
  * objects to the screen
  */
 public class Game extends SurfaceView implements SurfaceHolder.Callback {
+    private final Player player;
     private GameLoop gameLoop;
-    private Context context;
+
     public Game(Context context) {
         super(context);
 
@@ -24,11 +26,31 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
         SurfaceHolder surfaceHolder = getHolder();
         surfaceHolder.addCallback(this);
 
-        this.context = context;
         gameLoop = new GameLoop(this, surfaceHolder);
+
+        // Initialize player
+        player = new Player(getContext(), 500, 500, 30);
+
 
         setFocusable(true);
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        // Handle touch event actions
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                player.setPosition((double) event.getX(), (double) event.getY());
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                player.setPosition((double) event.getX(), (double) event.getY());
+                return true;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
         gameLoop.startLoop();
@@ -46,11 +68,13 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
         super.draw(canvas);
         drawUPS(canvas);
         drawFPS(canvas);
+
+        player.draw(canvas);
     }
     public void drawUPS(Canvas canvas) {
         String averageUPS = Double.toString(gameLoop.getAverageUPS());
         Paint paint = new Paint();
-        int color = ContextCompat.getColor(context, R.color.magenta);
+        int color = ContextCompat.getColor(getContext(), R.color.magenta);
         paint.setColor(color);
         paint.setTextSize(20);
         canvas.drawText("UPS: " + averageUPS, 100, 100, paint);
@@ -58,7 +82,7 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
     public void drawFPS(Canvas canvas) {
         String averageFPS = Double.toString(gameLoop.getAverageFPS());
         Paint paint = new Paint();
-        int color = ContextCompat.getColor(context, R.color.magenta);
+        int color = ContextCompat.getColor(getContext(), R.color.magenta);
         paint.setColor(color);
         paint.setTextSize(20);
         canvas.drawText("FPS: " + averageFPS, 100, 200, paint);
@@ -66,5 +90,6 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
 
     public void update() {
         // Update
+        player.update();
     }
 }
--- app/src/main/java/com/example/androidstudio2dgamedevelopment/MainActivity.java
+++ app/src/main/java/com/example/androidstudio2dgamedevelopment/MainActivity.java
@@ -16,13 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //Set window to fullscreen (will hide status bar)
-        Window window = getWindow();
-        window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
-
         //Set the content view to game so that objects in the Game class can be rendered to the screen
         setContentView(new Game(this));
     }
